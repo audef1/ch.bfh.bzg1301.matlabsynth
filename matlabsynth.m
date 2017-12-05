330872910c71cc0d0a4bdcd19dda42a306fc2263
Florian Auderset <f_auderset@hotmail.com>
2017-12-05 11:30:09 +0100

    added filechooser functionality

--- matlabsynth.m
+++ matlabsynth.m
@@ -1,12 +1,21 @@
 clear sound;
 
-global amplitude1
-global speed1
-
 synthgui()
 
 function synthgui
-
+    global file1
+    global track1
+    global y1
+    global Fs1
+    global t1
+    global amplitude1
+    global speed1
+    global track1player
+    
+    global gui
+    global playbtn
+    global stopbtn
+    
     % define track1 (static)
     file1 = 'track1.mp3';
     track1 = audioinfo(file1);
@@ -28,11 +37,11 @@ function synthgui
 
     loadTrack1 = uicontrol('Style','pushbutton','String','Load Track 1','Position',[315,225,70,25],'Callback', @loadTrack1_Callback);
 
-    play = uicontrol('Style','pushbutton','String','Play/Pause','Position',[315,200,70,25],'Callback', {@play_Callback,track1player});
-    stop = uicontrol('Style','pushbutton','String','Stop/Rewind','Position',[315,175,70,25],'Callback', {@stop_Callback,track1player});
+    playbtn = uicontrol('Style','pushbutton','String','Play','Position',[315,200,70,25],'Callback', @play_Callback);
+    stopbtn = uicontrol('Style','pushbutton','String','Stop/Rewind','Position',[315,175,70,25],'Callback', @stop_Callback);
 
     ha = axes('Units','Pixels','Position',[50,60,200,185]);
-    align([loadTrack1,play,stop,controlsText],'Center','None');
+    align([loadTrack1,playbtn,stopbtn,controlsText],'Center','None');
 
     % plot track 1
     plot(t1,y1*amplitude1);
@@ -45,20 +54,37 @@ function synthgui
 end
 
 function loadTrack1_Callback(hObject, eventdata, handles)
+    global track1
+    global y1
+    global Fs1
+    global track1player
+    global amplitude1
+    global speed1
+
     [FileName1,PathName] = uigetfile('*.mp3','Select a mp3 file');
-    track1 = audioinfo(FileName1);
-    [y1,Fs1] = audioread(FileName1);
+    
+    if FileName1 ~= 0
+        track1 = audioinfo(FileName1);
+        [y1,Fs1] = audioread(FileName1);
+        track1player = audioplayer(y1*amplitude1,Fs1*speed1);
+    end
 end
 
-function play_Callback(src, event, player)
-   if isplaying(player)
-       pause(player)
-   else
-       % play(player)
-       resume(player)
-   end
+function play_Callback(src, event)
+    global track1player
+    global playbtn
+
+	if isplaying(track1player)
+        playbtn.String = 'Play';
+        pause(track1player)
+    else
+        % play(track1player)
+        playbtn.String = 'Pause';
+        resume(track1player)
+	end
 end
 
-function stop_Callback(src, event, player)
-   stop(player)
+function stop_Callback(src, event)
+    global track1player
+    stop(track1player)
 end
